2819639146d4e33cbf20a1781dab0d7778eee4f0
Emily Kaplan <ekaplan@zymergen.com>
2020-07-17 10:31:13 -0700

    implemented cheese detail page

--- everycheese/cheeses/urls.py
+++ everycheese/cheeses/urls.py
@@ -5,9 +5,16 @@ from . import views
 app_name="cheeses"
 
 urlpatterns = [
+    # URL Pattern for the CheeseListView
     path(
         route='',
         view=views.CheeseListView.as_view(),
         name='list'
     ),    
+    # URL Pattern for the CheeseDetailView
+    path(
+        route='<slug:slug>/',
+        view=views.CheeseDetailView.as_view(),
+        name='detail'
+    )
 ]
