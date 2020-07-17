1ee6915435d6bc57214c13b0e73e64d38b70c4f3
Emily Kaplan <ekaplan@zymergen.com>
2020-07-17 13:41:12 -0700

    added creator to Cheese model

--- everycheese/cheeses/models.py
+++ everycheese/cheeses/models.py
@@ -4,6 +4,7 @@ from autoslug import AutoSlugField
 from django_countries.fields import CountryField
 from django.urls import reverse
 from model_utils.models import TimeStampedModel
+from django.conf import settings
 
 
 class Cheese(TimeStampedModel):
@@ -30,6 +31,12 @@ class Cheese(TimeStampedModel):
         "Country of Origin", blank=True
     )
 
+    creator = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        null=True,
+        on_delete=models.SET_NULL
+    )
+
 
     def __str__(self):
         return self.name
@@ -38,4 +45,6 @@ class Cheese(TimeStampedModel):
         return reverse(
             'cheeses:detail', kwargs={"slug": self.slug}
         )
+
+    
 
